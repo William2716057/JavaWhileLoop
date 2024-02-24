4670dfc4313cf9824c2e5fcbc886c55d3f5e7cb1
William <willspri@gmail.com>
2024-02-24 13:19:14 +1300

    file added

---
+++ WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+	public static void main(String[] args) {
+	int i = 1; //initialise the variable
+	
+	while (i < 10) { //set condition to end a 10
+		System.out.println(i); //print the value of 1 repeatedly
+		i++; //code to increment 
+	}
+}
+}
